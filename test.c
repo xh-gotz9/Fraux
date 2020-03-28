b8a39b16be118866a3151c1327c026c7e753b991
xh-gotz9 <xh353127981@gmail.com>
2020-03-28 11:10:28 +0800

    add torrent file and parse test
    ~~种子资源是诸神字幕组的空之境界1-8~~

--- test.c
+++ test.c
@@ -1,8 +1,28 @@
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
 #include <string.h>
 
 #include "bencode.h"
 #include "parser.h"
 #include "printer.h"
+
+#define ASSERT_RESULT(CONDITION)                  \
+    if (CONDITION == -1)                          \
+    {                                             \
+        printf("%s test failed!\n", #CONDITION);  \
+        exit(EXIT_FAILURE);                       \
+    }                                             \
+    else                                          \
+    {                                             \
+        printf("%s test success!\n", #CONDITION); \
+    }
 
 int create_node_test()
 {
@@ -44,6 +64,37 @@ int parse_data_list_only()
     }
 }
 
+int parse_data_from_file()
+{
+    int fd = open("./test.torrent", O_RDWR);
+    if (fd == -1)
+    {
+        perror("open error");
+        exit(EXIT_FAILURE);
+    }
+    struct stat stat;
+    if (fstat(fd, &stat) == -1)
+    {
+        perror("fstat error");
+        exit(EXIT_FAILURE);
+    }
+    off_t len = stat.st_size;
+    char *ptr = mmap(NULL, (size_t)len + 1, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+
+    ptr[len] = 0;
+
+    parser_buffer *buffer = create_parser_buffer(ptr);
+    bencode_node *node = parse_node(buffer);
+    munmap(ptr, len + 1);
+
+    if (node == NULL)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
 int write_print_buffer_test()
 {
     print_buffer *buffer = create_print_buffer();
@@ -55,17 +106,6 @@ int write_print_buffer_test()
     return strcmp(buffer->buf, "datadata") == 0 ? 0 : -1;
 }
 
-#define ASSERT_RESULT(CONDITION)                  \
-    if (CONDITION == -1)                          \
-    {                                             \
-        printf("%s test failed!\n", #CONDITION);  \
-        exit(EXIT_FAILURE);                       \
-    }                                             \
-    else                                          \
-    {                                             \
-        printf("%s test success!\n", #CONDITION); \
-    }
-
 int main(int argc, char *argv[])
 {
     ASSERT_RESULT(create_node_test());
@@ -73,7 +113,8 @@ int main(int argc, char *argv[])
     ASSERT_RESULT(parse_data_number_only());
     ASSERT_RESULT(parse_data_str_only());
     ASSERT_RESULT(parse_data_list_only());
-    
+    ASSERT_RESULT(parse_data_from_file());
+
     ASSERT_RESULT(write_print_buffer_test());
 
     return 0;
