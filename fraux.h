16680c6d72000e123c57bb4ab6d00ac33f3be07c
xh-gotz9 <xh353127981@gmail.com>
2020-04-28 00:49:03 +0800

    optimize code format

--- fraux.h
+++ fraux.h
@@ -53,21 +53,17 @@ enum
 };
 
 int fraux_parse(fraux_value *v, const char *bencode, size_t len);
-
 char *fraux_stringtify(fraux_value *v, size_t *length);
+
+void fraux_init(fraux_value *v);
+void fraux_clean(fraux_value *v);
 
 fraux_type fraux_get_type(fraux_value *v);
 
-void fraux_init(fraux_value *v);
-
-void fraux_clean(fraux_value *v);
-
+/* set value */
 void fraux_set_number(fraux_value *v, long int num);
-
 void fraux_set_string(fraux_value *v, const char *s, size_t len);
-
 void fraux_set_list(fraux_value *v, size_t capacity);
-
 void fraux_set_dictionary(fraux_value *v, size_t capacity);
 
 #endif
